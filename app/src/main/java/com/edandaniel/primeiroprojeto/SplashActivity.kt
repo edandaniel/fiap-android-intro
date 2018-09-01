3b68ed8f136b5d2693739fa5469f67479aba296a
Logon Aluno <logonrmlocal@fiap.com.br>
2018-09-01 09:04:11 -0300

    Initial commit

---
+++ app/src/main/java/com/edandaniel/primeiroprojeto/SplashActivity.kt
@@ -0,0 +1,27 @@
+package com.edandaniel.primeiroprojeto
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        load()
+    }
+
+    private fun load() {
+        Handler().postDelayed({
+                callnextScreen()
+        }, 2001L)
+    }
+
+    private fun callnextScreen() {
+        val nextScreenIntent = Intent(this, FormActivity::class.java)
+        startActivity(nextScreenIntent)
+        finish()
+    }
+}
